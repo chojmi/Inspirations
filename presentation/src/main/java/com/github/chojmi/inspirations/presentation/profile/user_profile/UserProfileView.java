321443f98a9648ecbea75c9088f259d6f17c6207
michal <michal.r.chojnacki@gmail.com>
2017-07-29 12:24:17 +0200

    Fixes displaying photos in user view

--- presentation/src/main/java/com/github/chojmi/inspirations/presentation/profile/user_profile/UserProfileView.java
+++ presentation/src/main/java/com/github/chojmi/inspirations/presentation/profile/user_profile/UserProfileView.java
@@ -1,7 +1,7 @@
 package com.github.chojmi.inspirations.presentation.profile.user_profile;
 
 import android.content.Context;
-import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -14,7 +14,7 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class UserProfileView extends CoordinatorLayout implements UserProfileContract.View {
+public class UserProfileView extends LinearLayoutCompat implements UserProfileContract.View {
     @Inject UserProfileContract.Presenter presenter;
     @BindView(R.id.user_name) TextView userName;
     @BindView(R.id.user_description) TextView description;
