e5b450f069b0f5ee1b88bf4f4da34109b0c9d298
michal <michal.r.chojnacki@gmail.com>
2017-09-09 16:31:52 +0200

    Fixes displaying gallery

--- presentation/src/main/java/com/github/chojmi/inspirations/presentation/utils/ImageViewUtils.java
+++ presentation/src/main/java/com/github/chojmi/inspirations/presentation/utils/ImageViewUtils.java
@@ -3,8 +3,6 @@ package com.github.chojmi.inspirations.presentation.utils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.github.chojmi.inspirations.presentation.R;
 
 public final class ImageViewUtils {
     private ImageViewUtils() {
@@ -12,7 +10,7 @@ public final class ImageViewUtils {
     }
 
     public static void loadImage(ImageView imageView, String url) {
-        Glide.with(imageView.getContext()).load(url).apply(RequestOptions.placeholderOf(R.drawable.ic_placeholder)).into(imageView);
+        Glide.with(imageView.getContext()).load(url).into(imageView);
     }
 
     public static void clearImageCache(ImageView imageView) {
