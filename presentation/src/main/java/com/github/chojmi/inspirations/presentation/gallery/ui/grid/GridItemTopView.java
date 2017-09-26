765de372e85f03a38678cd47357339acf3fb9835
michal <michal.r.chojnacki@gmail.com>
2017-09-26 16:29:43 +0200

    Adds progressbar when loading grid item image

--- presentation/src/main/java/com/github/chojmi/inspirations/presentation/gallery/ui/grid/GridItemTopView.java
+++ presentation/src/main/java/com/github/chojmi/inspirations/presentation/gallery/ui/grid/GridItemTopView.java
@@ -1,13 +1,21 @@
 package com.github.chojmi.inspirations.presentation.gallery.ui.grid;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutCompat;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.github.chojmi.inspirations.domain.entity.photos.PhotoSizeListEntity;
 import com.github.chojmi.inspirations.presentation.R;
 import com.github.chojmi.inspirations.presentation.gallery.model.PhotoWithAuthor;
@@ -21,7 +29,8 @@ import io.reactivex.Observable;
 import static com.github.chojmi.inspirations.presentation.utils.ImageViewUtils.clearImageCache;
 import static com.github.chojmi.inspirations.presentation.utils.ImageViewUtils.loadImage;
 
-public class GridItemTopView extends LinearLayoutCompat {
+public class GridItemTopView extends FrameLayout {
+    @BindView(R.id.progress_bar) ProgressBar progressBar;
     @BindView(R.id.photo) AspectRatioImageView photoHolder;
     @BindView(R.id.person_icon) ImageView personIconHolder;
     @BindView(R.id.title) TextView titleTextView;
@@ -30,7 +39,6 @@ public class GridItemTopView extends LinearLayoutCompat {
 
     public GridItemTopView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setOrientation(VERTICAL);
         LayoutInflater.from(context).inflate(R.layout.gallery_grid_top_item, this);
         ButterKnife.bind(this);
     }
@@ -39,7 +47,20 @@ public class GridItemTopView extends LinearLayoutCompat {
         if (photoSizeListEntity != null) {
             photoHolder.setAspectRatio(photoSizeListEntity.getRatio());
         }
-        loadImage(photoHolder, photo.getPhoto().getUrl());
+        toggleProgressBar(true);
+        loadImage(photoHolder, photo.getPhoto().getUrl(), new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                toggleProgressBar(false);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                toggleProgressBar(false);
+                return false;
+            }
+        });
         loadImage(personIconHolder, photo.getPerson().getIconUrl());
         titleTextView.setText(photo.getPhoto().getTitle());
         ownerTextView.setText(photo.getPerson().getUsername());
@@ -56,5 +77,9 @@ public class GridItemTopView extends LinearLayoutCompat {
 
     public Observable<Object> getPhotoClicksObservable() {
         return RxView.clicks(photoHolder);
+    }
+
+    private void toggleProgressBar(boolean isVisible) {
+        progressBar.setVisibility(isVisible ? View.VISIBLE : View.GONE);
     }
 }
