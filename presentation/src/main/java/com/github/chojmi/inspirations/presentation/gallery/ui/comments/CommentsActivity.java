04b7f321909b870c11824642e9bb1f834f035b0c
michal <michal.r.chojnacki@gmail.com>
2017-10-18 16:58:10 +0200

    Adds edittext and button for sending comments

--- presentation/src/main/java/com/github/chojmi/inspirations/presentation/gallery/ui/comments/CommentsActivity.java
+++ presentation/src/main/java/com/github/chojmi/inspirations/presentation/gallery/ui/comments/CommentsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -31,6 +33,8 @@ public class CommentsActivity extends BaseActivity implements CommentsContract.V
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
     @BindView(R.id.back) ImageButton backBtn;
     @BindView(R.id.title) TextView titleView;
+    @BindView(R.id.comment_text) EditText commentTextView;
+    @BindView(R.id.post_coment) Button postComment;
     @BindView(R.id.progress_bar) ProgressBar progressBar;
     @Inject CommentsContract.Presenter presenter;
     @Inject @Named("comments_adapter") BaseRecyclerViewAdapter<?, CommentEntity> adapter;
